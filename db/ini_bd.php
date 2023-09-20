66c1a366b671012b09980b4f756bf55eaaa41a8f
= <=>
2023-09-20 13:38:34 +0300

    Создание и удаление БД

---
+++ db/ini_bd.php
@@ -0,0 +1,23 @@
+<?php
+/* подключаем данные относительно корня каталога */
+$DB_SERVER_DATA = $_SERVER['DOCUMENT_ROOT'].'/data/db_server.php';
+
+include $DB_SERVER_DATA;
+
+
+/* Попытка подключения к серверу MySQL.*/
+$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD);
+// Проверяем соединение
+if($mysqli === false){
+ die("ERROR: Ошибка подключения: " . $mysqli->connect_error);
+}
+// Создание базы данных
+$sql = "CREATE DATABASE ".$DB_NAME;
+if($mysqli->query($sql) === true){
+  echo "База данных успешно создана";
+} else {
+  echo "Ошибка создания базы данных $sql. " . $mysqli->error;
+}
+// закрываем соединение
+$mysqli->close();
+?>
